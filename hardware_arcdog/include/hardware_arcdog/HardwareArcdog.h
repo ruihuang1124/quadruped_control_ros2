94a4efe581d45670af8750774d76be5b79f2d741
ruihuang1124 <ruihuang.scut@gmail.com>
2025-06-06 17:34:00 +0800

    add arcdog_hardware pkg

---
+++ hardware_arcdog/include/hardware_arcdog/HardwareArcdog.h
@@ -0,0 +1,55 @@
+//
+// Created by biao on 24-9-9.
+//
+
+#pragma once
+
+#include "hardware_interface/system_interface.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include "custom_msgs/msg/actuator_cmds.hpp"
+#include "custom_msgs/msg/mujoco_msg.hpp"
+#include "rt_usb_cdc.h"
+
+class HardwareArcdog final : public hardware_interface::SystemInterface
+{
+public:
+    CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
+
+    std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+    std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+    hardware_interface::return_type read(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+    hardware_interface::return_type write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) override;
+
+protected:
+    int tty_descriptor_;
+    int iterations_;
+    void imu_callback(const sensor_msgs::msg::Imu imu_state);
+    // void joint_state_callback(const sensor_msgs::msg::JointState joint_state);
+
+    // cmd
+    std::unordered_map<std::string, double> joint_position_commands_;
+    std::unordered_map<std::string, double> joint_velocity_commands_;
+    std::unordered_map<std::string, double> joint_effort_commands_;
+    std::unordered_map<std::string, double> joint_kp_commands_;
+    std::unordered_map<std::string, double> joint_kd_commands_;
+
+    // state
+    std::unordered_map<std::string, double> joint_position_states_;
+    std::unordered_map<std::string, double> joint_velocity_states_;
+    std::unordered_map<std::string, double> joint_effort_states_;
+    std::vector<double> imu_states_;
+    // std::vector<double> foot_contact_states_;
+
+    /*node*/
+    rclcpp::Node::SharedPtr node_;
+    /*publisher*/
+    // rclcpp::Publisher<custom_msgs::msg::ActuatorCmds>::SharedPtr actuator_cmd_publisher_;
+    /*subscriber*/
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscriber_;
+    // rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_subscriber_;
+};
