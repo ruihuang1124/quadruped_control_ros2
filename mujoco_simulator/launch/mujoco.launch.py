5077b31ef0d9c544400971c8471ecbb25bc8e1d8
ruihuang1124 <ruihuang.scut@gmail.com>
2025-07-09 20:44:33 +0800

    use sirius mid v3 as default

--- mujoco_simulator/launch/mujoco.launch.py
+++ mujoco_simulator/launch/mujoco.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     # 设置参数的默认值和描述
     robot_pkg = DeclareLaunchArgument(
         "robot_pkg",
-        default_value="sirius_description", # arcdog_description, piper_description sirius_description
+        default_value="sirius_mid_v3_description", # arcdog_description, piper_description sirius_description sirius_mid_v3_description
         description="package for robot description",
     )
 
--- robot_description/sirius_mid_v3_description/launch/visualize.launch.py
+++ robot_description/sirius_mid_v3_description/launch/visualize.launch.py
@@ -11,7 +11,7 @@ package_description = "sirius_mid_v3_description"
 
 def process_xacro():
     pkg_path = os.path.join(get_package_share_directory(package_description))
-    xacro_file = os.path.join(pkg_path, 'xacro', 'robot_modified.xacro')
+    xacro_file = os.path.join(pkg_path, 'xacro', 'robot.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     return robot_description_config.toxml()
 def generate_launch_description():
