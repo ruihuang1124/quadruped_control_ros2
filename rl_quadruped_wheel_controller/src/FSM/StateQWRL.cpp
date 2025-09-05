daada297c30562b3bdd27d7515121a05bb63305e
唐天扬 <ttyaigsw@gmail.com>
2025-09-05 18:06:42 +0800

    test: ready for real test

--- rl_quadruped_wheel_controller/src/FSM/StateQWRL.cpp
+++ rl_quadruped_wheel_controller/src/FSM/StateQWRL.cpp
@@ -138,7 +138,7 @@ void StateQWRL::enter()
     obs_.dof_vel = torch::zeros({1, params_.num_of_dofs});
     obs_.dof_vel_wheel = torch::zeros({1, 4});
     obs_.actions = torch::zeros({1, params_.num_of_dofs});
-    obs_.height = torch::tensor({{0.2}});
+    obs_.height = torch::tensor({{0.5}});
     // Init output
     output_torques = torch::zeros({1, params_.num_of_dofs});
     output_dof_pos_ = params_.default_dof_pos;
@@ -467,7 +467,7 @@ void StateQWRL::runModel()
             to(torch::kFloat).unsqueeze(0);
     }
     obs_.ang_vel = torch::tensor(robot_state_.imu.gyroscope).unsqueeze(0);
-    obs_.commands = torch::tensor({{control_.vel_x, control_.vel_y, control_.vel_yaw}});
+    obs_.commands = torch::tensor({{control_.vel_x, 0.0, 0.0}});
     torch::Tensor ee_pos = torch::tensor({control_.pos_x, control_.pos_y, control_.pos_z}).unsqueeze(0);
     torch::Tensor ee_ori = EulartoQuat(torch::tensor({control_.pos_roll, control_.pos_pitch, control_.pos_yaw}));
     // RCLCPP_INFO(node_->get_logger(), "command ee pose are, x: %.3f, y: %.3f, z: %.3f, r: %.3f, p: %.3f, y: %.3f",
@@ -482,7 +482,13 @@ void StateQWRL::runModel()
     obs_.dof_pos_leg = torch::tensor(robot_state_.motor_state.q).narrow(0, 0, 12).unsqueeze(0);
     obs_.dof_vel = torch::tensor(robot_state_.motor_state.dq).narrow(0, 0, params_.num_of_dofs).unsqueeze(0);
     obs_.dof_vel_wheel = torch::tensor(robot_state_.motor_state.dq).narrow(0, 12, 4).unsqueeze(0);
-    obs_.height = torch::tensor({{0.2}});
+    obs_.height = torch::tensor({{control_.vel_yaw}});
+    // float v = control_.vel_yaw;                   // 假设是标量
+    // v = std::clamp(v, 0.0f, 1.0f);
+    // float vd = std::floor(v * 10.0f) / 10.0f;
+
+    // // 若 obs_.height 需要 1D tensor
+    // obs_.height = torch::tensor({{vd}});
 
 
 
