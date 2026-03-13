fd3dc065d6bfd82f0fb464baad0b5b1f347a656e
wzmwzm6666-cmd <wzmwzm6666@gmail.com>
2026-03-13 16:05:35 +0800

    test2

--- rl_quadruped_adjustable_leg_controller/launch/real_robot_adjustable_leg.launch.py
+++ rl_quadruped_adjustable_leg_controller/launch/real_robot_adjustable_leg.launch.py
@@ -11,10 +11,10 @@ from launch_ros.substitutions import FindPackageShare
 from launch.actions import ExecuteProcess
 
 
-# //test123123123
 
 
-def launch_s123123etup(context, *args, **kwargs):
+
+def launch_setup(context, *args, **kwargs):
     package_description = context.launch_configurations['pkg_description']
     pkg_path = os.path.join(get_package_share_directory(package_description))
 
